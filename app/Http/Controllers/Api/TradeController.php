4968a17b5c0337d7ba503a0a104025504ac0920f
Mark Lester Bolotaolo <mlab817@gmail.com>
2022-03-03 11:47:39 +0800

    added port logic

--- app/Http/Controllers/Api/TradeController.php
+++ app/Http/Controllers/Api/TradeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TradeRequest;
+use App\Models\Portfolio;
 use App\Models\Trade;
 use Illuminate\Http\Request;
 
@@ -34,12 +35,72 @@ class TradeController extends Controller
      */
     public function store(TradeRequest $request)
     {
-        $trade = Trade::create($request->validated());
+        $request->validate([
+            'trade_type' => 'required|in:' . implode(',',Trade::TRADE_TYPES),
+            'company_id' => 'required|exists:companies,id',
+            'date' => 'required|date',
+            'shares' => 'required|int',
+            'price' => 'required|numeric',
+            'target_price' => 'required|numeric',
+            'cut_loss' => 'required|numeric',
+            'remarks' => 'nullable',
+        ]);
+
+        // validate first if transaction is sell
+        if ($request->trade_type == 'sell') {
+            $portfolio = auth()->user()->portfolios()->where('company_id', $request->company_id)->first();
+
+            if (! $portfolio) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'You do not own shares from this stock'
+                ], 200);
+            }
+
+            if ($portfolio->shares < $request->shares) {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'You do not own sufficient shares for that transaction',
+                ]);
+            }
+        }
+
+        $trade = Trade::create($request->all());
+
+        if ($request->trade_type == 'sell') {
+            $portfolio = auth()->user()->portfolios()->where('company_id', $request->company_id)->first();
+
+            $portfolio->update([
+                'shares' => $portfolio->shares - $request->shares
+            ]);
+        } else {
+            $portfolio = auth()->user()->portfolios()->where('company_id', $request->company_id)->first();
+
+            if ($portfolio) {
+                $portfolio->update([
+                    'shares' => $portfolio->shares + $request->shares
+                ]);
+            } else {
+                auth()->user()->portfolios()->save(new Portfolio([
+                    'company_id' => $request->company_id,
+                    'shares' => $request->shares
+                ]));
+            }
+        }
+
+        if (! auth()->user()->watchlists()->where('company_id', $request->company_id)->exists()) {
+            // add to watchlist
+            auth()->user()->watchlists()
+                ->create([
+                    'company_id'    => $request->company_id,
+                    'remarks'       => 'bought stock on ' . (string) $request->date,
+                ]);
+        }
 
         return response()->json([
             'success' => true,
             'data' => $trade
-            ]);
+        ]);
     }
 
     /**
