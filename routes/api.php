7a7409775cc9bdcd4faf2f03fdb433bd5afabfbe
Mark Lester Bolotaolo <mlab817@gmail.com>
2022-03-06 21:46:42 +0800

    removed sanctum

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    return response()->json(['user' => $request->user()]);
 });
 
 Route::get('/companies/{company}/historical_prices', function ($company) {
